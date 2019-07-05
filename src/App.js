ce81d59ca3b3c91aae4a76a6b35a51fe9f6d72c9
Sophia Brandt <tech@s-br.net>
2019-07-05 16:58:51 +0200

    📦 NEW: add sign-in with custom button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUp from './pages/sign-in-and-signup/sign-in-and-signup.component'
 import Header from './components/header/header.component'
 import './App.css'
 
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </>
   )
