c29fb06d27d610f8dd8ea0d185db73fe679d9ac2
Sophia Brandt <tech@s-br.net>
2019-07-06 21:16:32 +0200

    👌 IMPROVE: style code

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+
 import { auth } from '../../firebase/firebase.utils'
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
