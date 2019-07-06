97c9cd86fbb5556adf851ad2286992189a03597d
Sophia Brandt <tech@s-br.net>
2019-07-06 21:17:05 +0200

    📦 NEW: dispatchStateToProps in App.js, improve style code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import SignInAndSignUp from './pages/sign-in-and-signup/sign-in-and-signup.component'
-import Header from './components/header/header.component'
+import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './App.css'
 
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-signup/sign-in-and-signup.component'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
+
 class App extends React.Component {
-  constructor () {
-    super()
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount () {
+    const { setCurrentUser } = this.props
     auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           })
         })
       } else {
-        this.setState({ currentUser: null })
+        setCurrentUser(null)
       }
     })
   }
@@ -55,4 +50,11 @@ class App extends React.Component {
   }
 }
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
