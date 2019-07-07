8a87d8cabe6cbb5c4da2ef71b78d92c51ffb2992
Sophia Brandt <tech@s-br.net>
2019-07-07 21:03:57 +0200

    🐛 FIX: fix styling in cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,10 +8,11 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
-    {cartItems.map(cartItem => (
-      <CartItem key={cartItem.id} item={cartItem} />
-    ))}
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
