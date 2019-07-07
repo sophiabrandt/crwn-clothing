85946eb082800f12d7e4ee76037ac384c58a804c
Sophia Brandt <tech@s-br.net>
2019-07-07 10:13:04 +0200

    📦 NEW: add cart reducer and cart components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import CartIcon from '../cart-icon/cart-icon.component'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss'
+import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
-    <Link to='/' className='logo-container'>
+    <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/contact'>
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
@@ -32,11 +33,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
-)
+);
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
