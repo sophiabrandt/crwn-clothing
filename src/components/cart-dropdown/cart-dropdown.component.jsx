05b6683e293091007e71af48e981f27de9aa52c0
Sophia Brandt <tech@s-br.net>
2019-07-08 22:02:19 +0200

    🐛 FIX: add history to props in cart-dropdown.component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,7 +10,7 @@ import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
