eb8818672062f56dd2112e26c1b71001ad8f4dc5
Sophia Brandt <tech@s-br.net>
2019-07-11 17:12:57 +0200

    📦 NEW: add collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import CollectionItem from '../../components/collection-item/collection-item.component.jsx'
+
+import './collection.styles.scss'
+
+const CollectionPage = () => (
+  <div className='collection-page'>COLLECTION PAGE</div>
+)
+
+export default CollectionPage
