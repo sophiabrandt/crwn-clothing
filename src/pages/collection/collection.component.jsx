fc2f30a574bf7278ac2fba28c48ca4cbed7dcb3f
Sophia Brandt <tech@s-br.net>
2019-07-12 17:32:57 +0200

    📦 NEW: wire up selectCollection to match url params

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CollectionItem from '../../components/collection-item/collection-item.component.jsx'
 
+import { selectCollection } from '../../redux/shop/shop.selectors'
+
 import './collection.styles.scss'
 
-const CollectionPage = () => (
+const CollectionPage = ({ collection }) => (
   <div className='collection-page'>COLLECTION PAGE</div>
 )
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
