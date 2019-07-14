11cf243d8f85dd3eb066e31b11aabac54aee9478
Sophia <tech@s-br.net>
2019-07-14 15:48:04 +0200

    📦 NEW: add .env to gitignore

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
 
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 const CollectionsOverview = ({ collections }) => (
   <div className='shop-page'>
@@ -15,7 +15,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
