967a8028baca21cb85d4195d0583d768fa675400
Sophia Brandt <tech@s-br.net>
2019-07-16 12:43:29 +0200

    📦 NEW: add spinner HOC for loading state

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles'
+
+const withSpinner = WrappedComponent => ({ isLoading, ...props }) => {
+  return isLoading ? (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  ) : (
+    <WrappedComponent {...props} />
+  )
+}
+
+export default withSpinner
