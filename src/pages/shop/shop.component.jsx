8118cd695bd04f3576e6c9c29a8cfa5ed2b708d2
Sophia Brandt <tech@s-br.net>
2019-07-16 16:54:03 +0200

    👌 IMPROVE: refactor fetching collections to promise-based API

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,7 +28,7 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections')
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
       this.setState({ loading: false })
