f1106c3da21aea84617749c2365a680e5fe1815a
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-05-14 11:02:00 +0200

    👌 IMPROVE: improve code styling, add lazy loading/Suspense to shop page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense} from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -14,7 +14,9 @@ import { checkUserSession } from './redux/user/user.actions'
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
 const ShopPage = lazy(() => import('./pages/shop/shop.component'))
 const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
-const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+const SignInAndSignUpPage = lazy(() =>
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+)
 
 const App = ({ checkUserSession, currentUser }) => {
   React.useEffect(() => {
@@ -26,18 +28,18 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-    <Suspense fallback={<Spinner />}>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route exact path="/checkout" component={CheckoutPage} />
-      <Route
-      exact
-      path="/signin"
-      render={() =>
-        currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-      }
-      />
-      </Suspense>
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+        </Suspense>
       </Switch>
     </>
   )
@@ -51,7 +53,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
